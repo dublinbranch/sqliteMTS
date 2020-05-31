5d4a4e4a40eeea0a7f5bc8cab535ffb732005386
Roy Bellingan <roy@tech.techadsmedia.com>
2020-05-31 23:23:49 +0100

    in progress

--- sqlitemts.cpp
+++ sqlitemts.cpp
@@ -1,22 +1,61 @@
 #include "sqlitemts.h"
+#include "QStacker/qstacker.h"
 #include "sqlite3.h"
 #include <QDateTime>
-
-SqliteMTS::SqliteMTS() {
-}
+#include <QDebug>
 
 SqliteMTS::SqliteMTS(QByteArray db) {
-  connect(db);
+	connect(db);
 }
 
 int SqliteMTS::connect(QByteArray db) {
-  return this->db.connect(db.constData(), SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE);
+	auto status = this->db.connect(db.constData(), SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE);
+	this->db.enable_extended_result_codes();
+	return status;
 }
 
-std::unique_ptr<sqlite3pp::query> SqliteMTS::query(QByteArray sql) {
-  std::lock_guard<std::mutex>       lock_guard(mutex);
-  std::unique_ptr<sqlite3pp::query> res = std::make_unique<sqlite3pp::query>(this->db, sql);
-  return res;
+std::unique_ptr<sqlite3pp::query> SqliteMTS::fetch(QByteArray sql) {
+	std::lock_guard<std::mutex> lock_guard(mutex);
+	auto                        res = std::make_unique<sqlite3pp::query>(this->db);
+	try {
+		res->prepare(sql);
+		return res;
+	} catch (...) {
+	}
+	return res;
+}
+
+bool SqliteMTS::execute(QByteArray sql) {
+	std::lock_guard<std::mutex> lock_guard(mutex);
+	try {
+		this->db.execute(sql);
+
+		auto errNum = this->db.error_code();
+		if (errNum != 0) {
+			auto errMsg = this->db.error_msg();
+			qWarning().noquote() << "error in " << sql << errMsg << QStacker16Light();
+			return false;
+		}
+	} catch (...) {
+		return false;
+	}
+	return true;
+}
+
+bool SqliteMTS::execute(sqlite3pp::command& cmd) {
+	std::lock_guard<std::mutex> lock_guard(mutex);
+	try {
+		cmd.execute_all();
+		auto errNum = this->db.error_code();
+		if (errNum != 0 && errNum != 101) {
+			auto errMsg = this->db.error_msg();
+			qWarning().noquote() << "error in statement" << errMsg << QStacker16Light();
+			return false;
+		}
+	} catch (...) {
+		return false;
+	}
+	return true;
 }
 
 /**
@@ -25,37 +64,67 @@ std::unique_ptr<sqlite3pp::query> SqliteMTS::query(QByteArray sql) {
  * @param time
  * @return true: we can run, false: do not run
  */
-SqliteTool::SqliteTool(QByteArray db) {
-  this->sqlite.connect(db);
+SqliteTool::SqliteTool(const QByteArray db) {
+	this->sqlite.connect(db);
 }
 
 bool SqliteTool::runnable(const QString& key, qint64 second) {
-  static const QString skel = "SELECT lastRun FROM runnable WHERE operationCode = %1 ORDER BY lastRun DESC LIMIT 1";
-  auto                 now  = QDateTime::currentSecsSinceEpoch();
-  QByteArray           sql  = skel.arg(key).toUtf8();
-  auto                 res  = this->sqlite.query(sql);
-  for (auto v : *res) {
-    int lastRun;
-    v.getter() >> lastRun;
-    if (lastRun + second > now) {
-      return false;
-    }
-  }
-  return true;
+	prepareRunnable();
+	auto now = QDateTime::currentSecsSinceEpoch();
+	{
+		static const QString skel = "SELECT lastRun FROM runnable WHERE operationCode = '%1' ORDER BY lastRun DESC LIMIT 1";
+
+		QByteArray sql = skel.arg(key).toUtf8();
+		auto       res = this->sqlite.fetch(sql);
+		for (auto row : *res) {
+			int lastRun;
+			row.getter() >> lastRun;
+			if (lastRun + second > now) {
+				return false;
+			}
+		}
+	}
+	{
+		sqlite3pp::command cmd(this->sqlite.db, "INSERT INTO runnable (operationCode,lastRun) VALUES (?,?)");
+		auto               k = key.toUtf8();
+		cmd.binder() << k.constData() << now;
+		this->sqlite.execute(cmd);
+	}
+	return true;
 }
 
-bool SqliteTool::hasTable(QByteArray table)
-{
-    QByteArray sql = "SELECT name FROM sqlite_master WHERE type='table' AND name='" + table + "';";
-    auto res = this->sqlite.query(sql);
+bool SqliteTool::hasTable(QByteArray table) {
+	QByteArray sql = "SELECT name FROM sqlite_master WHERE type='table' AND name='" + table + "';";
+	auto       res = this->sqlite.fetch(sql);
+	if(!res->column_count()){
+		return false;
+	}
+	for (auto row : *res) {
+		std::string name;
+		row.getter() >> name;
+		if (name == table.toStdString()) {
+			return true;
+		}
+	}
+	return false;
 }
 
-bool SqliteTool::getRunnableOK() {
-  //this will just check  if the table exist, if not create it (used for new database)
-  if (runnableOK) {
-    return runnableOK;
-  }
-
-
-  return runnableOK;
+bool SqliteTool::prepareRunnable() {
+	//this will just check  if the table exist, if not create it (used for new database)
+	if (runnableOK) {
+		return runnableOK;
+	}
+	if (!hasTable("runnable")) {
+		auto sql = R"(
+CREATE TABLE `runnable` (
+     `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+     `operationCode` TEXT,
+     `lastRun` INTEGER
+);
+CREATE INDEX lastRun ON runnable(lastRun);
+)";
+		this->sqlite.execute(sql);
+	}
+	runnableOK = true;
+	return runnableOK;
 }
--- sqlitemts.h
+++ sqlitemts.h
@@ -1,33 +1,37 @@
-#ifndef SQLITEMTS_H
-#define SQLITEMTS_H
+#pragma once
+
+#ifndef QBL
+#define QBL(str) QByteArrayLiteral(str)
+#define QSL(str) QStringLiteral(str)
+#endif
 
 #include "sqlite3pp/src/sqlite3pp.h"
 #include <QString>
 #include <memory>
 #include <mutex>
 class SqliteMTS : sqlite3pp::noncopyable {
-public:
-  SqliteMTS();
-  SqliteMTS(QByteArray db);
-  int                               connect(QByteArray db);
-  std::unique_ptr<sqlite3pp::query> query(QByteArray sql);
+      public:
+	SqliteMTS()  {};
+	SqliteMTS(QByteArray db);
+	int                               connect(QByteArray db);
+	std::unique_ptr<sqlite3pp::query> fetch(QByteArray sql);
+	bool                              execute(QByteArray sql);
+	bool                              execute(sqlite3pp::command& cmd);
+	sqlite3pp::database               db;
 
-private:
-  std::mutex          mutex;
-  sqlite3pp::database db;
+      private:
+	std::mutex mutex;
 };
 
 class SqliteTool : sqlite3pp::noncopyable {
-public:
-  SqliteTool(QByteArray db);
-  bool runnable(const QString& key, qint64 second);
+      public:
+	SqliteTool(const QByteArray db);
+	bool runnable(const QString& key, qint64 second);
 
-  bool getRunnableOK();
-  bool hasTable(QByteArray table) const;
+	bool hasTable(QByteArray table);
 
-private:
-  SqliteMTS sqlite;
-  bool      runnableOK = false;
+      private:
+	SqliteMTS sqlite;
+	bool      runnableOK = false;
+	bool      prepareRunnable();
 };
-
-#endif // SQLITEMTS_H
